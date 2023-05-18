f6668c2f1de8099eeb2b593f33351956fc249e22
daniel <dyepesc@hotmail.com>
2023-05-17 20:47:16 -0500

    Se actualizan archivos HTML, CSS y JS

--- Picas&Fijas.js
+++ Picas&Fijas.js
@@ -2,47 +2,71 @@
 //                                  Programa para jugar picas y fijas                               */
 //************************************************************************************************* */
 
-//Crear numeros aleatorios
-
- function randomNumbers(min, max) {
-  const numbers = [];
-  for (let i = 0; i < 4; i++) {
-    numbers.push(Math.floor(Math.random() * (max - min) + min));
-  }
-  return numbers;
-}
-const numbers = randomNumbers(0, 9);
-document.getElementById("salida3").value = numbers; //los numeros aleatorios los muestra en el cuadro salida 3 del HTML
-console.log(numbers);  //Imprime los numeros aleeatorios en consola para validar código
-
-//************************************************************************************************* */
-
 //Crear variables
 
 const btn1 = document.querySelector('#btnenter'); //crea la variable btn utilizando el id de HTML (#=id)
 const btn2 = document.querySelector('#btnreset');
 const form1 = document.querySelector('#form1');
 const form2 = document.querySelector('#form2');
-//const resultado = document.querySelector('.container2');
 const resultado = document.querySelector('.container2'); // se crea la variable resultado para guardar el contenido de la clase container2 inactive de HTML
+const input3 = document.querySelector('#picas3'); // se crea input 3 para guardar el contenido de "#picas3"
+var matrix = [];
+var filas = 0;
 
 //************************************************************************************************* */
 
-    
+//Crear números aleatorios
+
+let numbers = [];
+  while(numbers.length < 4){
+      let r = Math.floor(Math.random() * 10);
+      if(numbers.indexOf(r) === -1) numbers.push(r);
+  }
+  for(var i=0; i<=filas; i++) {
+    matrix[i] = new Array(3);
+}
+
+  document.getElementById("salida3").value = numbers; //los numeros aleatorios los muestra en el cuadro salida 3 del HTML
+  console.log(numbers); //Imprime los numeros aleeatorios en consola para validar código
+
+//************************************************************************************************* */
+   
 // Configuracion de botón enter
+
+input3.addEventListener("keypress", function(e){
+  if (e.key === "Enter") {
+      event.preventDefault(); //Para que no haga reset
+      document.getElementById('btnenter').click();
+  }
+});
+
+//************************************************************************************************* */
+
+// Funcion para activar botón "jugar"
 
 btn1.addEventListener('click', darclick);
 
 function darclick() {
-  
-  const a = document.getElementById("picas3") // lo que escribo en el primer cuadro de texto se almacena en a
-  resultado.classList.replace('inactive', 'block'); //reemplaza el contenido de la clase .inactive que se encuentra en css por block.
-  const b = document.getElementById("mostrar") // se crea var b para mostrar en cuadro de texto "mostrar"
-  const c = Array.from(String(picas3.value), Number); // el numero escrito en picsa3 se convierte a un array de nombre c
-  b.innerHTML += c; // escribe el array c en b
-  document.getElementById("picas3").value = ''; // borra lo que se escribe en "picas3"
-}
+  const c = Array.from(String(picas3.value), Number); // el numero escrito en picas3 se convierte a un array de nombre c
+  if(validarRepeticion(c) && c.length ==4){
+    compararArrays(c);
+    document.getElementById("picas3").value = ''; // borra lo que se escribe en "picas3"
+    //let restric = parseInt(c.join(''));  //conviernte el array c en número y lo guarda en restric
+      let tblDatos = document.querySelector('.tabla').insertRow(1); //crea la tabla en HTML
+      let col1= tblDatos.insertCell(0);
+      let col2= tblDatos.insertCell(1);
+      let col3= tblDatos.insertCell(2);
 
+      col1.innerHTML +=c;
+      col2.innerHTML +=matrix[filas-1][1];
+      col3.innerHTML +=matrix[filas-1][2];
+    }
+  else{
+    alert("Por favor escriba únicamente 4 dígitos no repetidos");
+  }
+ 
+}
+ 
 //************************************************************************************************* */
 
 // Configuracion de botón reset
@@ -54,3 +78,46 @@ function darclick() {
   
   
 // }
+
+//************************************************************************************************* */
+
+
+// comparar los 2 arrays e imprimir resultado
+
+function compararArrays(c) {
+  let pica = 0;
+  let fija = 0;
+  for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] === c[i]) {
+      fija++;
+      
+  } else if (c.includes(numbers[i])) {
+      pica++;  
+      }
+          
+  }
+  matrix[filas][1]= pica;
+  matrix[filas][2]= fija;
+  filas++;
+  matrix[filas] = new Array(3);
+  if (fija == 4) {
+      alert("ganaste en " + filas + " movimientos - Juega de nuevo " + " \nEl numero es: " + numbers);
+      input3.disabled = true;
+      
+  }
+  
+}
+//************************************************************************************************* */
+
+// funcion que valida los numeros ingresados para que no se repitan
+
+function validarRepeticion(d) {
+  for (let f=0; f<d.length; f++){
+    for (let a= f+1; a<=d.length; a++){
+      if(d[f]==d[a]){
+        return false;
+      }
+    }
+  }
+  return true;
+  }
--- index-script.js
+++ index-script.js
@@ -1,25 +1,48 @@
 //Programa para jugar picas y fijas
 //Crear números aleatorios
 
-function randomNumbers(min, max) {
-    const numbers = [];
-    for (let i = 0; i < 4; i++) {
-      numbers.push(Math.floor(Math.random() * (max - min) + min));
-    }
-    return numbers;
-  }
+// function randomNumbers(min, max) {
+//     const numbers = [];
+//     for (let i = 0; i < 4; i++) {
+//       numbers.push(Math.floor(Math.random() * (max - min) + min));
+//     }
+//     return numbers;
+//   }
   
-  const numbers = randomNumbers(0, 9);
-  for (var a in numbers) {
+//   const numbers = randomNumbers(0, 9);
+//   for (var a in numbers) {
     
-  }
+//   }
  
-  console.log(numbers);
+//   console.log(numbers);
   
-  //Imprimir el numero que ingresa el usuario
-  b = 1234;
-  let c = Array.from(String(b), Number);
-  console.log(c)
+//   //Imprimir el numero que ingresa el usuario
+//   b = 1234;
+//   let c = Array.from(String(b), Number);
+//   console.log(c)
+
+//   /*-----------------------------------------------------------*/
+
+//   function compararArrays(numbers, c) {
+//     let pica = 0;
+//     let fija = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//       if (numbers[i] === c[i]) {
+//         fija++;
+//       } else if (c.includes(numbers[i])) {
+//         pica++;
+//       }
+//     }
+//     //return {pica, fija};
+   
+//     console.log("Picas: " + pica + " Fijas: " + fija)
+    
+//   }
+  
+  
+  //let array1 = [1, 2, 3];
+  //let array2 = [3, 2, 1];
+  //console.log(compararArrays(numbers, c)); // {pica: 3, fija: 0}
 
   //
 /*
@@ -32,6 +55,53 @@ for (let extraec in numbers){
   console.log(c[extraec]);
   }
   */
+
+ //------------------------------------------------------------------------------------
+// crea numeros aleatorios y no se repiten
+
+  // let arr = [];
+  // while(arr.length < 4){
+  //     let r = Math.floor(Math.random() * 10);
+  //     if(arr.indexOf(r) === -1) arr.push(r);
+  // }
+  // console.log(arr);
+
+//------------------------------------------------------------------------------------
+// compara que los numeros ingresados no se repitan
+
+// b = 1111;
+// const d = Array.from(String(b), Number);
+
+// console.log(d);
+// for (let f=0; f<d.length; f++){
+//   for (let a= f+1; a<=d.length; a++){
+//     if(d[f]==d[a]){
+//       console.log("repetido");
+//       a=d+1;
+//       f= d.length;
+//     }
+//   }
+// }
+// console.log("ok");
+
+// funcion que valida los numeros ingresados para que no se repitan
+
+b = 1111;
+const d = Array.from(String(b), Number);
+
+function norepetido(d) {
+
+for (let f=0; f<d.length; f++){
+  for (let a= f+1; a<=d.length; a++){
+    if(d[f]==d[a]){
+      console.log("repetido");
+      a=d+1;
+      f= d.length;
+    }
+  }
+}
+return f;
+}
 
 
   
